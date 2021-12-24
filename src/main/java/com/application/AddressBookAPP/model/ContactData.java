d4f4e1bad93f68c0b9e2e68ca4483f5a83c2ba6a
KunigiMohan <kmohanr5@gmail.com>
2021-12-24 14:00:36 +0530

    Added : Model Class to assign ID to created ContactDTO Class.

---
+++ src/main/java/com/application/AddressBookAPP/model/ContactData.java
@@ -0,0 +1,36 @@
+package com.application.AddressBookAPP.model;
+
+import com.application.AddressBookAPP.dto.ContactDTO;
+
+public class ContactData {
+    private int iD;
+    private ContactDTO contactDTO;
+    
+    /**
+     * Defining Constructer
+     * @param iD
+     * @param contactDTO
+     */
+    public ContactData(int iD, ContactDTO contactDTO) {
+        this.iD = iD;
+        this.contactDTO = contactDTO;
+    }
+
+    public int getiD() {
+        return iD;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public ContactDTO getContactDTO() {
+        return contactDTO;
+    }
+
+    public void setContactDTO(ContactDTO contactDTO) {
+        this.contactDTO = contactDTO;
+    }
+
+    
+}
